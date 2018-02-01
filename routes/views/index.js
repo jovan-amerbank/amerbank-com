d1033d2b5ea12e53530913949606b6e5aedcfb91
jovandamjanovic <jovand@amerbank.com>
2018-02-01 11:00:57 +0100

    blog section connected to frontpage

--- routes/views/index.js
+++ routes/views/index.js
@@ -8,7 +8,27 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+
+	locals.data = {
+		posts: []
+	};
+
+	view.on('init', function (next) {
+
+		var keystone = require('keystone'),
+		Post = keystone.list('Post');
+	 
+		Post.model.find()
+			.where('state', 'published')
+			.populate('author')
+			.sort('-publishedAt')
+			.limit(3)
+			.exec(function(err, posts) {
+				locals.data.posts = posts;
+				next(err);
+			});
+	});
+
 	// Render the view
 	view.render('index');
 };
